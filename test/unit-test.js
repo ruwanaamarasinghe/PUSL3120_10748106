784be0e34bcbab01d3d1a8f6d7c59983331e78cd
Ruwana Amarasinghe <ruwanaamarasinghe@gmail.com>
2023-01-30 17:22:25 +0530

    updated unit-test.js
    unit test for database connection

--- test/unit-test.js
+++ test/unit-test.js
@@ -1,8 +1,22 @@
 let chai = require("chai");
 var request = require('request');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/FullStackDB');
 
+//server connection
 it('Main page content', function(done) {
     request('http://localhost:8080' , function(error, response, body) {
         done();
     });
+});
+
+//mongoose database connection
+it('Main page content', function(done) {
+    mongoose.connection
+    .once('open', () => console.log('Connected!'))
+    .on('error', (error) => {
+        console.warn('Error : ', error);
+    });
+    done();
 });
